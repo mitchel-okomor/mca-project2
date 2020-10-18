9916ea7fd92f9e93cb93ce7fcbe7e99f8c78f363
CodeXaver <mitchel.okomor@gmail.com>
2020-10-18 17:59:09 +0100

    changed toggle button

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function toggleNav(){
     console.log("clicked");
     var el = document.getElementById("toggle-items");
+    var btn = document.getElementById("toggle-button");
     if(el.classList.contains("remove-menu")){
         el.classList.remove("remove-menu");
+       btn.innerHTML = "X";
     }else{
        el.classList.add("remove-menu");
- 
+ btn.innerHTML = `<svg viewBox="5 0 50 100" width="40" height="40">
+       <rect y ="21" width="60" height="6"></rect>
+       <rect y="40" width="60" height="6"></rect>
+       <rect y="60" width="60" height="6"></rect>
+     </svg>`;
     }
 }
